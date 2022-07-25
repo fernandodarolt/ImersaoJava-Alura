7f51350d1a2ad18623d03f3a0f47ea76acc670fd
Fernando da Rolt <fernandodarolt@hotmail.com>
2022-07-24 23:00:01 -0300

    #ImersaoJava

--- src/App.java
+++ src/App.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //String url = ("https://imdb-api.com/en/API/Top250Movies/k_gioc78zk");
-        //ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
-        String url = ("https://api.nasa.gov/planetary/apod?api_key=RhL2omY8EO2BPnSU8UmSCOfLvzYhIBVjEoEvFKbU&start_date=2022-06-12&end_date=2022-06-14");
-        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+//        String url = ("http://imdb-api.com/en/API/Top250Movies/k_gioc78zk");
+//        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+        //String url = ("https://api.nasa.gov/planetary/apod?api_key=RhL2omY8EO2BPnSU8UmSCOfLvzYhIBVjEoEvFKbU&start_date=2022-06-12&end_date=2022-06-14");
+        //ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+        String url = ("http://localhost:8080/linguagens");
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+
         var http = new ClienteHttp();
         String json = http.buscaDados(url);
 
@@ -16,7 +19,7 @@ public class App {
         for (int i = 0; i < 3; i++){
             Conteudo conteudo = conteudos.get(i);
             InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
-            String nomeArquivo = "/saida" + conteudo.getTitulo()+".png";
+            String nomeArquivo = conteudo.getTitulo()+".png";
             geradora.criar(inputStream, nomeArquivo);
             System.out.println("\u001b[38;5;15mTitulo:\u001b[0m " + "\u001b[38;5;2m" + conteudo.getTitulo());
         }
